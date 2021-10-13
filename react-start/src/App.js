997dc629ee63d733304542ca326b7c9016cd259c
Anton9512 <anton.mail9512@gmail.com>
2021-10-13 11:40:18 +0300

    added route everywhereand NavLink

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -3,7 +3,10 @@ import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
 import {BrowserRouter, Route} from "react-router-dom"
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 
 const  App = () => {
@@ -15,6 +18,9 @@ const  App = () => {
         <div className="app-wrapper-content">
           <Route path="/dialogs" component={Dialogs}/>
           <Route path="/profile" component={Profile}/>
+          <Route path="/news" component={News}/>
+          <Route path="/music" component={Music}/>
+          <Route path="/settings" component={Settings}/>
         </div>
       </div>
     </BrowserRouter>
