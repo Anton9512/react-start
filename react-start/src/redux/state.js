82a61f0d265946c98a462f0ec71c1cda6e74d2a4
Anton9512 <anton.mail9512@gmail.com>
2021-10-25 15:02:29 +0300

    added dispatch and action

--- react-start/src/redux/state.js
+++ react-start/src/redux/state.js
@@ -33,6 +33,9 @@ let store = {
    _callSubscriber() {
       console.log(' ')
    },
+   subscribe(observer) {
+      this._callSubscriber = observer;
+   },
    addPost() {
       let newPost = {
          id: 5,
@@ -47,9 +50,22 @@ let store = {
       this._state.profilePage.newPostText = newText;
       this._callSubscriber(this._state);
     },
-   subscribe(observer) {
-      this._callSubscriber = observer;
-   }
+   dispatch(action) {
+      if (action.type === 'ADD-POST') {
+         let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+         };
+         this._state.profilePage.postData.push(newPost);
+         this._state.profilePage.newPostText = '';
+         this._callSubscriber(this._state);
+      } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+         this._state.profilePage.newPostText = action.newText;
+         this._callSubscriber(this._state);
+      }
+   },
+   
 };
 
  export default store;
