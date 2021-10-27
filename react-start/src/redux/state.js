80ac76df58414be72485bf8d7b1c47121de1889f
Anton9512 <anton.mail9512@gmail.com>
2021-10-27 11:48:36 +0300

    added action creator and action type

--- react-start/src/redux/state.js
+++ react-start/src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
    _state: {
       profilePage: {
@@ -51,24 +54,31 @@ let store = {
       this._callSubscriber(this._state);
     },
    dispatch(action) {
-      if (action.type === 'ADD-POST') {
+      if (action.type === ADD_POST) {
          let newPost = {
             id: 5,
             message: this._state.profilePage.newPostText,
             likesCount: 0
          };
-         this._state.profilePage.postData.push(newPost);
+         this._state.profilePage.postData.unshift(newPost);
          this._state.profilePage.newPostText = '';
          this._callSubscriber(this._state);
-      } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+      } else if (action.type === UPDATE_NEW_POST_TEXT) {
          this._state.profilePage.newPostText = action.newText;
          this._callSubscriber(this._state);
       }
    },
    
 };
+export const addPostActionCreator = () => {
+   return {type: ADD_POST}
+ };
+ 
+export const updateNewPostTextActionCreator = (text) => {
+   return {type: UPDATE_NEW_POST_TEXT, newText: text}
+ };
 
- export default store;
+export default store;
 
 
  //profilePage - обслуживает одну страницу, messagePage - другую
