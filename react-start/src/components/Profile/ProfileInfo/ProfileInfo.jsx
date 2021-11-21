5582cfc9d2b3d3dd06420e1e37bb80488392599a
Anton9512 <anton.mail9512@gmail.com>
2021-11-21 11:52:51 +0300

    setState, local state

--- react-start/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react-start/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from'./ProfileInfo.module.css';
 import Preloader from '../../common/preloader/Preloader';
 import userPhoto from '../../../images/user.png';
+import ProfileStatus from './ProfileStatus';
 
 class ProfileInfo extends React.Component{
   constructor(props) {
@@ -17,22 +18,8 @@ class ProfileInfo extends React.Component{
             <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"></img>
           </div> */}
           <img className={s.userPhoto} src={this.props.profile.photos.large || userPhoto} alt="" />
-          <div className={s.userdata}>
-            {/* <div className={s.imageblock}>
-              <img className={s.image} src="https://www.meme-arsenal.com/memes/22a3c0a7f871b62553164d795da369d9.jpg"></img>
-            </div> */}
-            <div className={s.userinfo}>
-              {/* <div className={s.userinfo__title}>Anton P.</div> */}
-              <div className={s.userName}>{this.props.profile.fullName}</div>
-              <div className={s.userinfo__info}>
-                <span>Status: {this.props.profile.status}</span>
-                <span>About me: "{this.props.profile.aboutMe}"</span>
-                <div>Contacts:</div>
-                <span>Web Site: нет</span>
-              </div>
-            </div>
-          </div>
-      </div>
+          <ProfileStatus status={'hello bro!'}/>
+        </div>
       )
   }
 };
