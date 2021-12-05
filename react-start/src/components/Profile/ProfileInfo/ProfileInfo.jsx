c2b5a31fd5cc810a39fbcc5ef4234b086cc3942a
Anton9512 <anton.mail9512@gmail.com>
2021-12-05 15:40:14 +0300

    hook, useState

--- react-start/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react-start/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import s from'./ProfileInfo.module.css';
 import Preloader from '../../common/preloader/Preloader';
 import userPhoto from '../../../images/user.png';
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 class ProfileInfo extends React.Component{
   constructor(props) {
@@ -18,7 +19,7 @@ class ProfileInfo extends React.Component{
             <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"></img>
           </div> */}
           <img className={s.userPhoto} src={this.props.profile.photos.large || userPhoto} alt="" />
-          <ProfileStatus status={this.props.status} updateStatus={this.props.updateStatus}/>
+          <ProfileStatusWithHooks status={this.props.status} updateStatus={this.props.updateStatus}/>
         </div>
       )
   }
