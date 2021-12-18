4a71220c459eaf0acd86ff6cf44a1abd0bfd15da
Anton9512 <anton.mail9512@gmail.com>
2021-12-18 12:49:32 +0300

    update profile

--- react-start/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react-start/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -18,10 +18,31 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
         <div>
           <img className={s.userPhoto} src={profile.photos.large || userPhoto} alt="" />
             <div>{isOwner && <input type={'file'} onChange={onMainPhotoSelected}/>}</div>
+            <ProfileData profile={profile}/>
           <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
         </div>
       )
 };
+
+
+const ProfileData = ({profile}) => {
+    return <div>
+        <div><b>Full name</b>: {profile.fullName}</div>
+        <div><b>Looking for a job</b>: {profile.lookingForAJob ? 'yes' : 'no'}</div>
+        { profile.lookingForAJob &&
+        <div><b>My professional skills</b>: {profile.lookingForAJobDescription}</div>
+        }
+        <div><b>About me </b>: {profile.aboutMe}</div>
+        <div><b>Contacts </b>: {Object.keys(profile.contacts).map(key => {
+            return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+        })}</div>
+    </div>
+}
+
+const Contact = ({contactTitle, contactValue}) => {
+    return <div className={s.contact}><b>{contactTitle}</b>: {contactValue}</div>
+
+}
 
 
 export default ProfileInfo;
