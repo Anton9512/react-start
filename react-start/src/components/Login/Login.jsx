719ec706a352c0cc1398b5f5596bc95550fabab9
Anton9512 <anton.mail9512@gmail.com>
2021-12-21 13:13:21 +0300

    captcha

--- react-start/src/components/Login/Login.jsx
+++ react-start/src/components/Login/Login.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import LoginForm from './LoginForm';
-import { reduxForm } from 'redux-form';
-import { connect } from "react-redux";
-import { login } from "../../redux/auth-reducer";
-import { Redirect } from "react-router";
+import {reduxForm} from 'redux-form';
+import {connect} from "react-redux";
+import {login} from "../../redux/auth-reducer";
+import {Redirect} from "react-router";
 
 const Login = (props) => {
-   const onSubmit = (formData) => {
-      props.login(formData.email, formData.password, formData.rememberMe)
-   }
+    const onSubmit = (formData) => {
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha)
+    }
 
-   if (props.isAuth) {
-      return <Redirect to={'/profile'}/>
-   };
+    if (props.isAuth) {
+        return <Redirect to={'/profile'}/>
+    }
+    ;
 
-   return (
-      <div>
-         <h1>Login</h1>
-         <LoginReduxForm onSubmit={onSubmit}/>
-      </div>
-   )
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl}/>
+        </div>
+    )
 };
-const LoginReduxForm =  reduxForm({
-   form: 'login',                     
- })(LoginForm);
+const LoginReduxForm = reduxForm({
+    form: 'login',
+})(LoginForm);
 
 
 const mapStateToProps = (state) => ({
-   isAuth: state.auth.isAuth
+    isAuth: state.auth.isAuth,
+    captchaUrl: state.auth.captchaUrl,
 })
 
-export default connect(mapStateToProps, {login}) (Login); 
+export default connect(mapStateToProps, {login})(Login);
--- react-start/src/components/Login/LoginForm.jsx
+++ react-start/src/components/Login/LoginForm.jsx
@@ -5,12 +5,15 @@ import { Input } from "../common/FormsControls/FormsControls";
 import s from './Login.module.css';
 import style from './../common/FormsControls/FormsControls.module.css'
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
    return (
-      <form onSubmit={handleSubmit} action="">
+      <form onSubmit={handleSubmit}>
          <div><Field validate={[required]} placeholder={'Email'} name={'email'} type="text" component={Input}/></div>
          <div><Field validate={[required]} placeholder={'Password'} name={'password'}  type="password" component={Input}/></div>
          <div><Field type={'checkbox'} name={'rememberMe'}  component={Input}/> remember me</div>
+          { captchaUrl && <img src={captchaUrl} alt=""/>}
+          { captchaUrl && <Field placeholder={'symbols from image'} name={'captcha'} validate={[required]}  component={Input}/>}
+
          {error && <div className={style.formSummuryError}>
             {error}
          </div>}
